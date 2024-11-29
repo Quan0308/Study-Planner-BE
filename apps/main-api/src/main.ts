206badc160c6b197d1ca3980944c61234d82d223
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-11-29 11:43:54 +0700

    ci: use docker for deploy

--- apps/main-api/src/main.ts
+++ apps/main-api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformResponseInterceptor(new Reflector()));
   app.useGlobalInterceptors(new ExceptionHandlerInterceptor());
   app.useGlobalPipes(ThrowFirstErrorValidationPipe);
-  const port = process.env.PORT || 80;
+  const port = process.env.PORT || 8000;
   await app.listen(port);
   Logger.log(`🚀 Main application is running on: http://localhost:${port}/${globalPrefix}`);
 }
